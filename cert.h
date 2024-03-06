51c8f1d9fb2b61db546eb72de8b241f2a259eee1
vinhnguyenhoanggia <162393269+vinhnguyenhoanggia@users.noreply.github.com>
2024-03-06 14:04:52 +0700

    Add files via upload

---
+++ cert.h
@@ -0,0 +1,13 @@
+
+
+#define CERT_H
+
+const char * rootCACertificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW\n"
+"QyNTUxOQAAACCD66JPFtcagOf/+sWNKeQnKsh7yikeZi3zS2CnCQIFaAAAAJhMh7PzTIez\n"
+"8wAAAAtzc2gtZWQyNTUxOQAAACCD66JPFtcagOf/+sWNKeQnKsh7yikeZi3zS2CnCQIFaA\n"
+"AAAEAevv1zhL/RRGqgNHv1ltfiYjanyH5ZPIHutYcslUeSwIProk8W1xqA5//6xY0p5Ccq\n"
+"yHvKKR5mLfNLYKcJAgVoAAAAEWF2cjAwMDVAZ21haWwuY29tAQIDBA==\n"
+"-----END CERTIFICATE-----\n";
+
